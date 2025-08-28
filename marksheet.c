765c8cc5bacbfe52b45e37c4f1c9a95d53ac3ad6
kandeeepan-cell <kandeeepan@gmail.com>
2025-08-28 00:16:55 -0700

    Add files via upload

---
+++ marksheet.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  float marks[5];	
+  marks[0]=90;
+  marks[1]=90;
+  marks[2]=70;
+  marks[3]=92;
+  marks[4]=90;
+  if("averagea>40");
+  if("result==true");
+  printf("pass");
+  printf("fail");
+       printf("\t\tBCA_A\n");
+printf("\t______________________\n");
+       printf("name:kandeepan\t\t"); 
+	   printf("register no:12345\n");
+printf("\t_______________________\n");
+       printf("english=%5.2f\n",marks[0]);
+       printf("tamil=%5.2f\n",marks[1]);
+       printf("maths=%5.2f\n",marks[2]);
+       printf("science=%5.2f\n",marks[3]);
+       printf("social=%5.2f\n",marks[4]);
+printf("\t_______________________\n");
+       printf("\ttotal\t\t\t:%5.2f\t\t\t result=%5.2f\n");
+       getch();
+}
